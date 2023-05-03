23a20c45ac9033381abec76d3657f5e4fb872652
Jahid-Unlock <jahid.nits@gmail.com>
2023-05-03 16:12:44 +0600

    20%

--- app/components/IdeaCard.jsx
+++ app/components/IdeaCard.jsx
@@ -1,21 +1,27 @@
 'use client'
 
 const IdeaCard = ({text, tag, username, date}) => {
-
- 
+  
+  const parseDate = ()=>{
+    const options = { day: 'numeric', month: 'long', year: 'numeric' };
+    const formattedDate = new Date(date).toLocaleDateString('en-US', options);
+    return formattedDate
+  }
 
   return (
     <div className="md px-5 py-4 ring-2 rounded-md basis-[23.7%] flex flex-col justify-between">
         <div>
             <div className="flex justify-between">
                 <div><span className='text-[13px] inline-block px-4 py-[4px] bg-blue-500 text-white rounded-full'>{tag}</span></div>
-                <div><button onClick={()=> console.log('11')} className="text-[13px] h-6 w-6 rounded-full bg-red-700 text-white underline"><i class="bi bi-three-dots-vertical"></i></button></div>
+                <div><button onClick={()=> console.log('11')} className="text-[13px] h-6 w-6 rounded-full bg-red-700 text-white underline"><i className="bi bi-three-dots-vertical"></i></button></div>
             </div>
-
-            <p className="py-3">{text}</p>
+            <p className="py-4">{text}</p>
         </div>
 
-        <p className="text-sm text-blue-600">@{username}</p>
+        <div className="flex justify-between items-center">
+          <p className="text-sm text-blue-600">@{username}</p>
+          <p className="text-sm text-gray-700">{parseDate()}</p>
+        </div>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -18,14 +18,14 @@ export default async function Home() {
 
   return (
     <main>
-      <div className="container max-w-[1200px] px-3 py-6 mx-auto">
+      <div className="container max-w-[1200px] px-3 py-7 mx-auto">
         <div className="flex justify-between items-center gap-3 flex-wrap">
           <h4 className="text-2xl font-semibold">Share Your Random Ideas  &#128513;</h4>
-          <AddIdea label={'Add Your Idea +'}/>
+          <AddIdea label={'Add Your Idea +'} />
         </div>
         <div className="flex gap-5 flex-wrap mt-10">
-          {data.data.map((item)=>(
-            <IdeaCard key={item.id} text={item.text} date={item.date} username={item.username} tag={item.tag}/>
+          {data.data.map((item) => (
+            <IdeaCard key={item.id} text={item.text} date={item.date} username={item.username} tag={item.tag} />
           ))}
         </div>
       </div>
